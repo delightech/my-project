a2c0e08db3f5081b07bda4f2e9ba80ae22677b3f
delightech <hisafumi.kikkawa@gmail.com>
2020-09-08 09:37:43 +0900

    ADD vue router

---
+++ src/views/Count.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- :xxx=" " はv-bind:xxx=" " のシンタックスシュガー -->
+    <!-- :xxx=" " のダブルクォートの中はjsとして解釈される -->
+    <Counter name="Counter 1" :initCount="5" @emitUp="getEvent" />
+    <Counter name="Counter 2" :initCount="10" @emitUp="getEvent" />
+    {{ stack }}
+  </div>
+</template>
+<script>
+import Counter from '@/components/Counter.vue'
+
+export default {
+  components: {
+    Counter
+  },
+  data() {
+    return {
+      stack: []
+    }
+  },
+  methods: {
+    getEvent(payload) {
+      this.stack.push(payload)
+    }
+  }
+}
+</script>
